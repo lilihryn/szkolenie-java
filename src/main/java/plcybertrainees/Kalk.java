915641ffb398802f1d79592b16b031348630cc8c
lilihryn <108538332+lilihryn@users.noreply.github.com>
2022-07-11 11:02:19 +0200

    ok

--- src/main/java/plcybertrainees/Kalk.java
+++ src/main/java/plcybertrainees/Kalk.java
@@ -1,5 +1,27 @@
 package plcybertrainees;
+import java.util.Scanner;
 
 public class Kalk {
+    static double main(String... args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Zmianna1?");
+        double zmianna1 = sc.nextDouble();
+        System.out.println("Zmianna2?");
+        double zmianna2 = sc.nextDouble();
+        System.out.println("Jaky operator?");
+        String operator = sc.next();
+        double resultat = 0.0;
+        if (operator "==" ' "+") {
+            resultat = zmianna1 + zmianna2;
+            if(operator '==' "-");
+            resultat=zmianna1-zmianna2;
+            if(operator == "/");
+            resultat=zmianna1/zmianna2;
+            else if(operator == "*");
+            resultat=zmianna1*zmianna2;
 
+        };
+
+        return resultat;
+    }
 }
