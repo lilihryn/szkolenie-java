80f5c499377df3db89faff75d78de32459fdd3cb
lilihryn <108538332+lilihryn@users.noreply.github.com>
2022-07-13 20:21:47 +0200

    OKOK

--- src/main/java/plcybertrainees/SimpleApp.java
+++ src/main/java/plcybertrainees/SimpleApp.java
@@ -1,13 +1,11 @@
 package plcybertrainees;
 
-import com.fasterxml.jackson.databind.deser.std.StringArrayDeserializer;
-
 public class SimpleApp {
     public static String SEPARATOR_SPACE= " ";
     public static void main(String[] args) {
         String a="Glory";
         String b= "to";
-        String c= "Ukraine";
+        String c= "Ukraine11";
 
 
 System.out.println(a+SEPARATOR_SPACE+b+SEPARATOR_SPACE+c);
