83a00a0f6efdf64dc94563b078a5342dd700a4e2
lilihryn <108538332+lilihryn@users.noreply.github.com>
2022-07-13 20:21:23 +0200

    OKOK

--- src/main/java/plcybertrainees/SimpleApp.java
+++ src/main/java/plcybertrainees/SimpleApp.java
@@ -1,16 +1,25 @@
 package plcybertrainees;
-
-import com.fasterxml.jackson.databind.deser.std.StringArrayDeserializer;
-
+import plcybertrainees.dziedziczenie.Kolor;
+import plcybertrainees.dziedziczenie.model;
 public class SimpleApp {
-    public static String SEPARATOR_SPACE= " ";
+
+
     public static void main(String[] args) {
-        String a="Glory";
-        String b= "to";
-        String c= "Ukraine";
+        model modelPojazdu1 = new model("VW","2021","przod","Bug",
+                1200,2.1,Kolor.NIEBIESKI);
+        var modelPojazdu2 = new model("VW", "2020",
+                "przod", "Passat", 1000,
+                2.0, Kolor.ZIELONY);
+        var modelPojazdu3 = new model("VW", "2020",
+                "przod",
+                "Passat", 3000, 2.0,
+                Kolor.ZIELONY);
 
 
-System.out.println(a+SEPARATOR_SPACE+b+SEPARATOR_SPACE+c);
-
+        System.out.println(modelPojazdu1.toString());
+        System.out.println(modelPojazdu2);
+        System.out.println(modelPojazdu1.getMarka());
+        System.out.println(modelPojazdu1.getKolor());
+        System.out.println(modelPojazdu1.getNaped());
     }
 }
---
+++ src/main/java/plcybertrainees/dziedziczenie/pojazd.java
@@ -0,0 +1,36 @@
+package plcybertrainees.dziedziczenie;
+
+public class pojazd {
+    /* nowa klasa(marka pojazdu) */
+    private String marka;
+    private String rocznik;
+    private String naped;
+    public pojazd(){
+        marka="";
+        rocznik="";
+        naped ="";
+    }
+//alt+fn+del=constructor menu//
+
+    public pojazd(String marka, String rocznik, String naped) {
+        this.marka = marka;
+        this.rocznik = rocznik;
+        this.naped = naped;
+    }
+    public String toString(){
+        return "marka:"+this.marka
+                +"rocznik:"+this.rocznik
+                +"napęd:"+this.naped;
+
+        }
+    public String getMarka(){
+        return marka;
+    }
+    public String getRocznik(){
+        return rocznik;
+    }
+    public String getNaped(){
+        return naped;
+    }
+    //gettery metody ktore pobieraja informacje z polklasy dla przetwarzania//
+}
