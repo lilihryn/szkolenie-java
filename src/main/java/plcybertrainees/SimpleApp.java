898ba9e221395ffe95d66bee6182bebf9cb4e399
lilihryn <108538332+lilihryn@users.noreply.github.com>
2022-07-22 20:06:24 +0200

    opis struktury klas i metod w java

--- src/main/java/plcybertrainees/SimpleApp.java
+++ src/main/java/plcybertrainees/SimpleApp.java
@@ -1,5 +1,15 @@
+//1.Konstrukcja klas i metod
 package plcybertrainees;
+//1. Konstrukcja klass i metod package pl.cyber.trainees;
+// oreślenie pakietu wramach projektu, gdzie //
+// znajduje się nasza klasa. //pole package ustawia się automatycznie jeśli nasz plik zawiera się w odpowiedniej strukturze
+// katalogów tj src/main/java/......
+// w naszym przykładzie src/main/java/pl.cyber.trainees
 
+//region miejsca na importy
+//poprzez import dajemy informacje ze z konkretnej biblioteky mozemy korzystac w naszej aplikacji
+//import.java.math*; importować bez wyjątky
+//endregion
 public class SimpleApp {
     public static void main(String[] args) {
 
